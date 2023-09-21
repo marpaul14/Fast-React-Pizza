b45342f05366b8351976c911883c91e032f6a14e
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-09-21 11:26:12 +0800

    components was managed using react router

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <Link to="/">Fast React Pizza Co.</Link>
+
+        <p>Marpaul</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
