00d7f5d7c29fef7a0ba44e60df1eff10d6c3d2a6
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-09-26 10:13:54 +0800

    redux tool kit

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -41,12 +41,38 @@ const cartSlice = createSlice({
   },
 });
 
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState,
+//   reducers: {
+//     addItem(state, action) {
+//       state.cart.push(action.payload);
+//     },
+//     deleteItem(state, action) {
+//       state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+//     },
+//     increaseItemQuantity(state, action) {
+//       const item = state.cart.find((item) => item.pizzaId === action.payload);
+//       item.cart++;
+//       item.totalPrice = item.quantity * item.unitPrice;
+//     },
+//     decreaseItemQuantity(state, action) {
+//       const item = state.cart.find((item) => item.pizzaId === action.payload);
+//       item.cart--;
+//       item.totalPrice = item.quantity * item.unitPrice;
+//     },
+//     clearCart(state, action) {
+//       state.cart = [];
+//     },
+//   },
+// });
+
 export const {
   addItem,
   deleteItem,
-  increaseQuantity,
+  increaseItemQuantity,
   decreaseItemQuantity,
   clearCart,
-} = cartSlice.actions();
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
