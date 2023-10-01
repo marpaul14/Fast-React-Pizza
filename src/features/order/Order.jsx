265d295e8ef7b0f0fd5cb54b7ee92aeb3de5dba2
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-01 19:56:07 +0800

    useFetcher hook

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -77,7 +77,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
