3f10a1fcd5ae6b7df3bafa0d4e5eaf62f1aba4b9
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-04 21:17:52 +0800

    final

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
       justify-between border-b border-stone-200 bg-yellow-400 px-4 
       py-3 uppercase sm:px-6"
       >
-        <Link to="/" className="tracking-widest">
+        <Link to="/Fast-React-Pizza" className="tracking-widest">
           Fast React Pizza Co.
         </Link>
         <SearchOrder />
