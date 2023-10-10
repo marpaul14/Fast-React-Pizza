4efd420b35df14fc01ab72e2d3ff24f8ba49e44b
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-10 22:41:02 +0800

    renaming

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  base: "/Fast-React-Pizza",
+  base: "/Getyo-Pizza",
 });
