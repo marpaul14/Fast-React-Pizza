d2ba57862b036a555c91a04a5881362ca94c27f8
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-10 22:56:52 +0800

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/Fast-React-Pizza",
+        path: "/Getyo-Pizza",
         element: <Home />,
       },
       {
